1606ad0d2ff3eb3b9943d65fa9162e2c283d87f4
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-07-14 18:20:19 +0300

    add parameterized tests

--- src/test/java/compiler/CompilerTest.java
+++ src/test/java/compiler/CompilerTest.java
@@ -4,27 +4,40 @@ import command.Command;
 import command.impl.*;
 import exception.BrainfuckCompileCommandException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CompilerTest {
+    private static Compiler compiler = new Compiler();
 
-    @Test
-    void testCompile(){
-
-        String program = "?";
-        Compiler compiler = new Compiler();
+    @ParameterizedTest
+    @ValueSource(strings = {"?", "/", "@"})
+    void testShouldThrowExceptionForInvalidCommandCompile(String instruction){
 
         Exception exception = assertThrows(BrainfuckCompileCommandException.class, () ->
-                compiler.compile(program));
-        assertEquals("Invalid command: " + program, exception.getMessage());
+                compiler.compile(instruction));
+        assertEquals("Invalid command: " + instruction, exception.getMessage());
+    }
 
-        String secondProgram = "+-><.[]";
+    @ParameterizedTest
+    @ValueSource(strings = {"+", "-", ">", "<", ".", "[]"})
+    void testShouldReturnCompiledCommand(String instruction){
 
-        List<Command> compiledCommands = compiler.compile(secondProgram);
-        assertFalse(compiledCommands.isEmpty());
+        List<Command> compiledCommands = compiler.compile(instruction);
+        assertEquals(1,compiledCommands.size());
+        assertTrue(Command.class.isAssignableFrom(compiledCommands.get(0).getClass()));
+    }
+
+    @Test
+    void testChecksCompiledCommandsOrder(){
+        String program = "+-><.[]";
+
+        List<Command> compiledCommands = compiler.compile(program);
+
         assertEquals(6,compiledCommands.size());
         assertSame(compiledCommands.get(0).getClass(), IncrementValue.class);
         assertSame(compiledCommands.get(1).getClass(), DecrementValue.class);
